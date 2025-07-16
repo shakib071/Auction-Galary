dcfac91ec98d7df869b5f819c53f5da4e524b4ba
shakib071 <shakibhasan071@gmail.com>
2025-07-17 03:47:36 +0600

    responsive nav and banner

--- Auction_Galary/src/components/Banner.jsx
+++ Auction_Galary/src/components/Banner.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 const Banner = () => {
     return (
-        <div className=' bg-[url("./assets/images/Banner-min.jpg")] bg-cover bg-center h-full px-5 py-20 w-full text-white mx-auto'>
+        <div className=' bg-[url("./assets/images/Banner-min.jpg")] bg-cover bg-center h-full px-3 py-9 sm:px-10 sm:py-20 xl:py-25 xl:px-15 w-full text-white mx-auto'>
             
             <div className='w-[50%]'>
-                <p className='text-2xl font-medium'>Bid on Unique Items from Around the World</p>
-                <p className='mt-3 font-[300] text-[14px]'>Discover rare collectibles, luxury goods, and vintage treasures in our curated auctions</p>
-                <button className='cursor-pointer mt-5  border-1 px-4 py-1 rounded-xl text-black bg-white'> Explore Auctions</button>
+                <p className='text-sm sm:text-xl lg:text-4xl xl:text-5xl 2xl:text-6xl font-medium'>Bid on Unique Items from Around the World</p>
+                <p className='mt-3 font-[300] text-[11px] sm:text-[14px] lg:text-xl xl:text-2xl 2xl:text-3xl'>Discover rare collectibles, luxury goods, and vintage treasures in our curated auctions</p>
+                <button className='cursor-pointer text-[10px] sm:text-sm lg:text-lg xl:text-xl 2xl:text-2xl mt-5  border-1 px-4 py-1 lg:py-2 rounded-xl text-black bg-white'> Explore Auctions</button>
             </div>
             
 
--- Auction_Galary/src/components/Navbar.jsx
+++ Auction_Galary/src/components/Navbar.jsx
@@ -5,25 +5,25 @@ import { IoMdNotificationsOutline } from "react-icons/io";
 const navbar = () => {
     return (
         
-        <div className='w-[95%] mx-auto'>
+        <div className='w-[98%] sm:w-[95%] mx-auto'>
             
-            <div className='flex justify-between items-center py-3'>
+            <div className='flex justify-between items-center py-2 sm:py-3'>
 
                 <div>
-                    <p className='text-xl font-medium  text-[rgba(0,62,164,1)]'>Auction<span className='text-[rgba(255,211,55,1)]'>Galary</span></p>
+                    <p className='text-[11px] sm:text-xl lg:text-2xl xl:text-4xl font-medium  text-[rgba(0,62,164,1)]'>Auction<span className='text-[rgba(255,211,55,1)]'>Galary</span></p>
                 </div>
 
-                <div className='flex justify-center gap-4 text-[16px]] items-center'>
+                <div className='pl-2 md:pl-0 flex justify-center gap-2 sm:gap-6 text-[10px] sm:text-[16px] lg:text-lg xl:text-xl items-center'>
                     <p>Home</p>
                     <p>Auctions</p>
                     <p>Categories</p>
                     <p>How to works</p>
                 </div>
 
-                <div className='flex justify-center items-center gap-2 '>
+                <div className='flex justify-center items-center gap-2 sm:gap-3 '>
                   
-                  <p className='text-3xl'><IoMdNotificationsOutline /> </p>
-                  <p className='text-[27px]'><FaRegUserCircle /></p>
+                  <p className='text-2xl lg:text-3xl xl:text-4xl'><IoMdNotificationsOutline /> </p>
+                  <p className='text-xl lg:text-[27px] xl:text-4xl'><FaRegUserCircle /></p>
                 </div>
 
             </div>
